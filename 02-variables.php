43ac2f7843f3a37b7636bd0071e77511d51b04f0
Brunback <131183013+Brunback@users.noreply.github.com>
2023-08-27 23:15:21 -0500

    Inicio de fundamento de php

---
+++ 02-variables.php
@@ -0,0 +1,20 @@
+<?php include 'includes/header.php';
+
+$nombre="Esteban";
+
+$nombre = "Esteban2";
+
+echo $nombre;
+var_dump($nombre);
+
+define('constante', "este es el valor de la constante ");
+echo constante;
+
+const constante2 = "Hola 2";
+echo constante2;
+
+
+$nombreCliente= "pedro";
+
+echo $nombreCliente;
+include 'includes/footer.php';
---
+++ 04-operadores.php
@@ -0,0 +1,28 @@
+<?php include 'includes/header.php';
+
+$numero1 = 20;
+$numero2 = 10;
+
+//sumar
+ echo $numero1 + $numero2;
+ echo"<br>";
+
+//Restar
+echo $numero1 - $numero2;
+echo"<br>";
+
+//Multiplicar 
+echo $numero1 * $numero2;
+echo"<br>";
+
+//Dividir 
+echo $numero1/$numero2;
+echo"<br>";
+
+//Multiplicar cierta cantidad de veces
+echo 2 ** 6;
+
+
+
+
+include 'includes/footer.php';
