9e3420183251c2a6b6223b3c8d16920e548f049e
Brunback <131183013+Brunback@users.noreply.github.com>
2023-08-28 16:55:11 -0500

    Funciones , tipados , require y manejo de json

--- 15-funciones.php
+++ 15-funciones.php
@@ -1,7 +1,19 @@
-<?php include 'includes/header.php';
+<?php 
+declare(strict_types=1);
+include 'includes/header.php';
 
+function sumar(int $numero1, int $numero2){
+  echo $numero1 + $numero2;
+}
 
+sumar(4,2);
+echo "<br>";
 
+sumar(50, 100);
+echo "<br>";
+
+sumar(numero2:26, numero1:54);
+echo "<br>";
 
 
 include 'includes/footer.php';
--- 18.json.php
+++ 18.json.php
@@ -1,8 +1,41 @@
-<?php include 'includes/header.php';
+<?php 
+declare(strict_types=1);
+include 'includes/header.php';
 
+$productos = [
+    [
 
+          'nombre' => 'Reloj',
+          'precio' => 150,
+          'disponible' => true
+    
+    ],
+    [
+        'nombre' => 'Televisor 24"',
+        'precio' => 400,
+        'disponible' => true
+    
+    
+    ],
+    [
+        'nombre' => 'Procesador',
+        'precio' => 200,
+        'disponible' => false
+    
+    ]
 
+];
 
+echo "<pre>";
+var_dump($productos);
+
+$json = json_encode($productos, JSON_UNESCAPED_UNICODE);
+
+$json_array = json_decode($json);
+
+var_dump($json);
+var_dump($json_array);
+echo"</pre>";
 
 
 include 'includes/footer.php';
